ce6d0c895dcfe2c4786ebef0221115f41589d5b5
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-28 21:40:43 +0100

    read html select options from application.propeties for field country

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,19 +1,26 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String studentForm(Model model) {
 
         model.addAttribute("student", new Student("Tim", "Buchalka", "Germany"));
+        model.addAttribute("countries", countries);
 
         return "student-form";
     }
