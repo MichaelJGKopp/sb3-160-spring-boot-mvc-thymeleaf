90a12f7a8661ca2da276821a73f7642d7daaca13
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-01-28 21:23:50 +0100

    add country field to Student with select tag

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -4,10 +4,8 @@ import com.luv2code.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
@@ -15,7 +13,7 @@ public class StudentController {
     @GetMapping("/showStudentForm")
     public String studentForm(Model model) {
 
-        model.addAttribute("student", new Student("Tim", "Buchalka"));
+        model.addAttribute("student", new Student("Tim", "Buchalka", "Germany"));
 
         return "student-form";
     }
@@ -23,7 +21,8 @@ public class StudentController {
     @PostMapping("/processStudentForm")
     public String processStudentForm(@ModelAttribute("student") Student student) {
 
-        System.out.println("student: " + student.getFirstName() + " " + student.getLastName());
+        System.out.println("student: " + student.getFirstName() + " " + student.getLastName()
+                + " from " + student.getCountry());
 
         return "student-confirmation";
     }
